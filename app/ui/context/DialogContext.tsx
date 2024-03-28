680c9eafb380b13a0c95fb3ae88855517b97df6e
johnson <poshenc@gmail.com>
2024-03-29 01:00:52 +0800

    add: dialog context api

---
+++ app/ui/context/DialogContext.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import Dialog from "@mui/material/Dialog/Dialog"
+import { PropsWithChildren, ReactNode, createContext, useState } from "react"
+
+interface Store {
+    openDialog(content: ReactNode): void
+    closeDialog(): void
+}
+
+const DialogContext = createContext<Store>({} as Store)
+
+const DialogProvider: React.FC<PropsWithChildren> = ({ children }) => {
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [content, setContent] = useState<ReactNode>('')
+
+    const openDialog = (content: ReactNode) => {
+        setIsDialogOpen(true)
+        setContent(content)
+    }
+
+    const closeDialog = () => {
+        setIsDialogOpen(false)
+    }
+
+    const dialogContext: Store = {
+        openDialog: openDialog,
+        closeDialog: closeDialog
+    }
+
+    return (
+        <DialogContext.Provider value={dialogContext}>
+            <Dialog open={isDialogOpen} onClose={closeDialog}>
+                {content}
+            </Dialog>
+            {children}
+        </DialogContext.Provider>
+    )
+}
+
+export { DialogContext, DialogProvider };
+
