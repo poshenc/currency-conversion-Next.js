ae368435579308582f777e1a81362b7a7393d0b5
johnson <poshenc@gmail.com>
2024-03-30 18:32:11 +0800

    add: mui icon package

--- app/ui/components/Header/Header.tsx
+++ app/ui/components/Header/Header.tsx
@@ -1,16 +1,19 @@
-import Button from "../Button/Button";
+import IconButton from "../IconButton/IconButton";
+import TextButton from "../TextButton/TextButton";
+import styles from "./Header.module.css";
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Header({
   title,
   onClose,
 }: {
   title: string,
-  onClose: () => void,
+  onClose?: () => void,
 }) {
   return (
-    <>
-      <div>{title}</div>
-      <Button onClick={onClose}>X</Button>
-    </>
+    <div className={styles.container}>
+      <h1>{title}</h1>
+      {onClose && <IconButton className={styles.button} onClick={onClose}><CloseIcon /></IconButton>}
+    </div>
   )
 }
