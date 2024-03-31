8fe0c120fe643f51c38cb2772362ba2340f21d0b
johnson <poshenc@gmail.com>
2024-03-31 10:19:19 +0800

    modify: enhance input component with search functionality

--- app/ui/components/Input/Input.tsx
+++ app/ui/components/Input/Input.tsx
@@ -2,17 +2,22 @@ import React, { ChangeEvent, FunctionComponent, InputHTMLAttributes, useState } 
 import styles from './Input.module.css';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  onValueChange(value: string): void
+  onValueChange(value: string): void;
+  Icon?: React.ElementType;
+  textAlign?: 'left' | 'right';
 }
 
-const Input: FunctionComponent<Props> = ({ onValueChange, ...props }) => {
+const Input: FunctionComponent<Props> = ({ onValueChange, Icon, textAlign = 'left', ...props }) => {
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     onValueChange(event.target.value)
   }
 
   return (
-    <input className={styles.input} {...props} onChange={onChangeHandler} />
+    <div className={styles.container}>
+      <input id='input-box' className={styles.input} style={{ textAlign }} {...props} onChange={onChangeHandler} />
+      {Icon && <label htmlFor='input-box'><Icon className={styles.icon} /></label>}
+    </div>
   )
 }
 
